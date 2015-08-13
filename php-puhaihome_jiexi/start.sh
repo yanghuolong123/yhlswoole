8730ccacaa747cc943824f9df5487b10eb1a5ae6
yanghuolong <yanghuolong@2fe2bff7-f9c6-4b43-8d6f-6fb8de716474>
2015-08-13 15:55:30 +0000

    #952700

--- php-puhaihome_jiexi/start.sh
+++ php-puhaihome_jiexi/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo kill -s 9 `ps -aux | grep puhai_home_server.php | awk '{print $2}'`
+sudo kill -s 9 `ps -aux | grep puhai_home_server.php | grep -v grep | awk '{print $2}'`
 
 php puhai_home_server.php
