f233498933d341734a42d1751f6ac755f2cac978
yanghuolong <yanghuolong@2fe2bff7-f9c6-4b43-8d6f-6fb8de716474>
2015-08-13 15:55:11 +0000

    #952700

--- heartbeat_server/start.sh
+++ heartbeat_server/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo kill -s 9 `ps -aux | grep heartbeat_server.php | awk '{print $2}'`
+sudo kill -s 9 `ps -aux | grep heartbeat_server.php | grep -v grep | awk '{print $2}'`
 
 php heartbeat_server.php
